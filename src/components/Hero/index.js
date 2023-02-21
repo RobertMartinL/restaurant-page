2236b619490ed9b4e026aa80e104e58fe95a77ff
Robert Martin <rlmartin1996@gmail.com>
2023-02-21 11:49:15 -0600

    background image/hero darkened

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import burgerHero from '../../assets/burgerHero.jpg'
+import './hero.css'
 
 const Hero = () => {
   return (
     <div className='heroContainer'>
         <img className='heroImg' src={burgerHero} alt='Hamburger on a skillet'/>
+        <div className='overlay'></div>
     </div>
   )
 }
